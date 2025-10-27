d9ee1f4cf6a506892d01e28d997c909b89f6dfb1
dec161 <162108835+dec161@users.noreply.github.com>
2025-10-27 22:05:36 +0300

    Добавил NailWarehouse.App/Infrastructure/EnumExtensions.cs

---
+++ NailWarehouse.App/Infrastructure/EnumExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace NailWarehouse.App.Infrastructure;
+
+internal static class EnumExtensions
+{
+    public static string GetDisplayName(this Enum value)
+    {
+        var valueString = value.ToString();
+
+        var attribute = value.GetType().GetMember(valueString).FirstOrDefault()
+            ?.GetCustomAttribute(typeof(DisplayAttribute)) as DisplayAttribute;
+        
+        return attribute?.Name ?? valueString;
+    }
+}
