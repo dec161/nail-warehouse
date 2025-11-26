70e8458e0dda89b1b023258bb13e5e4bab23564b
dec161 <162108835+dec161@users.noreply.github.com>
2025-11-26 14:30:06 +0300

    Создал NailWarehouse.EntityExtensions

---
+++ BL/NailWarehouse.EntityExtensions/NailExtensions.cs
@@ -0,0 +1,15 @@
+﻿using NailWarehouse.Entities.Models;
+
+namespace NailWarehouse.EntityExtensions;
+
+/// <summary>
+/// Расширения для <see cref="Nail"/>.
+/// </summary>
+public static class NailExtensions
+{
+    /// <summary>
+    /// Считает общую сумму товара.
+    /// </summary>
+    public static decimal CalculateTotalPrice(this Nail nail) =>
+        nail.Amount * nail.Price;
+}
