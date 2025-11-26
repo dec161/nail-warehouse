8ff30951a2bba09af9e9b38202343384d5a99a9e
dec161 <162108835+dec161@users.noreply.github.com>
2025-11-26 13:32:49 +0300

    Добавил NailWarehouse.EntityManager.Contracts

---
+++ BL/NailWarehouse.EntityManager.Contracts/INailManager.cs
@@ -0,0 +1,29 @@
+﻿using NailWarehouse.Entities.Models;
+
+namespace NailWarehouse.EntityManager.Contracts;
+
+/// <summary>
+/// Интерфейс для управления хранилищем.
+/// </summary>
+public interface INailManager
+{
+    /// <summary>
+    /// Добавляет товар во внутреннее хранилище.
+    /// </summary>
+    Task Add(Nail nail, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Получает список всех товаров в хранилище.
+    /// </summary>
+    Task<IEnumerable<Nail>> GetAll(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Удаляет товар из внутреннего хранилища.
+    /// </summary>
+    Task Remove(Nail nail, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Получает статистику о товарах.
+    /// </summary>
+    Task<NailStatistics> GetStatistics(CancellationToken cancellationToken = default);
+}
