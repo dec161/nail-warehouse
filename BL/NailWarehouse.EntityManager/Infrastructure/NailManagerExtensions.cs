2177fa1db103ff0904b39d1412573c7004207a3d
dec161 <162108835+dec161@users.noreply.github.com>
2025-11-30 15:44:04 +0300

    Создал NailManagerExtensions

---
+++ BL/NailWarehouse.EntityManager/Infrastructure/NailManagerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using NailWarehouse.EntityManager.Contracts;
+
+namespace NailWarehouse.EntityManager.Infrastructure;
+
+/// <summary>
+/// Расширения для <see cref="NailManager"/>.
+/// </summary>
+public static class NailManagerExtensions
+{
+    /// <summary>
+    /// Добавляет логирование производительности
+    /// к методам <paramref name="nailManager"/>.
+    /// </summary>
+    public static INailManager WithPerformanceLogging(
+        this INailManager nailManager,
+        ILogger<TimedNailManager> logger) =>
+        new TimedNailManager(nailManager, logger);
+}
