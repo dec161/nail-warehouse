2fd1d23ad33fc4f24aa1e9f136a20cc809231e8b
dec161 <162108835+dec161@users.noreply.github.com>
2025-12-09 10:33:17 +0300

    Создал NailDatabaseContext

---
+++ DAL/NailWarehouse.DatabaseStorage/NailDatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NailWarehouse.Entities.Models;
+
+namespace NailWarehouse.DatabaseStorage;
+
+/// <inheritdoc/>
+public class NailDatabaseContext : DbContext
+{
+    /// <summary>
+    /// Сущность <see cref="Nail"/>.
+    /// </summary>
+    public DbSet<Nail> Nails { get; set; }
+
+    /// <summary>
+    /// Создаёт экземпляр <see cref="NailDatabaseContext"/>.
+    /// </summary>
+    public NailDatabaseContext() =>
+        Database.EnsureCreated();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSqlServer(
+            @"Server=(localdb)\mssqllocaldb;Database=NailDatabase;Trusted_Connection=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+        modelBuilder.Entity<Nail>(builder => builder.ComplexProperty(nail => nail.Size));
+}
