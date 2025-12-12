76ac02aacbf46bca577f94d21e9d7ec8fc8f0348
dec161 <162108835+dec161@users.noreply.github.com>
2025-12-12 16:36:39 +0300

    Убрал лишний код в HomeController

--- NailWarehouse.MvcApp/Controllers/HomeController.cs
+++ NailWarehouse.MvcApp/Controllers/HomeController.cs
@@ -5,19 +5,7 @@ using System.Diagnostics;
 namespace NailWarehouse.MvcApp.Controllers;
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
     {
         return View();
     }
