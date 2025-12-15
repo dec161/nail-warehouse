9ae570e8445a2628b0618396b7281f08caf066ae
dec161 <162108835+dec161@users.noreply.github.com>
2025-12-15 20:00:48 +0300

    Добавил summary в HomeController.cs

--- NailWarehouse.MvcApp/Controllers/HomeController.cs
+++ NailWarehouse.MvcApp/Controllers/HomeController.cs
@@ -1,15 +1,25 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using NailWarehouse.MvcApp.Models;
 using System.Diagnostics;
 
 namespace NailWarehouse.MvcApp.Controllers;
+
+/// <summary>
+/// Контроллер для главной страницы.
+/// </summary>
 public class HomeController : Controller
 {
+    /// <summary>
+    /// Отображает главную страницу.
+    /// </summary>
     public IActionResult Index()
     {
         return View();
     }
 
+    /// <summary>
+    /// Отображает страницу ошибки.
+    /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
